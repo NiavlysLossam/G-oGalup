727d2dc7a62887ee471bc8c7ea0f8c29b89462ac
NiavlysLossam <sylvain.massol@gmail.com>
2026-02-19 22:24:33 +0100

    refactor: Inline azimuth map widget JavaScript into its template, enabling Alt/Shift-click for azimuth setting with an arrow icon.

--- photos/widgets.py
+++ photos/widgets.py
@@ -1,6 +1,7 @@
 from django.contrib.gis.forms.widgets import OSMWidget
 
 class AzimuthMapWidget(OSMWidget):
+    template_name = 'photos/admin/azimuth_map.html'
     
     class Media:
         js = (
